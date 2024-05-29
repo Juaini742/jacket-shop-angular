e25addacd32cdaa017bb850c3b9c9303322f18fa
Juaini742 <juaini742@gmail.com>
2024-05-29 21:25:13 +0800

    Remove environment.ts from version control

--- frontend/src/app/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  public_api_url: 'http://localhost:8080/api/public',
-  secured_api_url: 'http://localhost:8080/api/secured',
-  stripe_publishable_key:
-    'pk_test_51OpVNxF9OLaL2OugKcpOle4fDph5umpwDO0LKHslP14C81U63nDAX9FsAxzLjGXiK5HwBqQyCmfVeb5W2GFYNzl600jf3I3s1D',
-};
